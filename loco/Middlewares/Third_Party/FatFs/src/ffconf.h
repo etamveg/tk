7fbad465fb6925ebc35ae917c6344b16a056da1d
Tamas Vegh <vegh-tamas@hotmail.com>
2017-07-22 22:28:31 +0200

    fordul

--- loco/Middlewares/Third_Party/FatFs/src/ffconf.h
+++ loco/Middlewares/Third_Party/FatFs/src/ffconf.h
@@ -258,7 +258,7 @@
 
 #define _FS_REENTRANT           1
 #define _FS_TIMEOUT             1000
-#define	_SYNC_t                 osSemaphoreId
+#define	_SYNC_t                 uint8_t
 /* The _FS_REENTRANT option switches the re-entrancy (thread safe) of the FatFs
 /  module itself. Note that regardless of this option, file access to different
 /  volume is always re-entrant and volume control functions, f_mount(), f_mkfs()
